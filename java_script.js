eae32c220c561c67bb480c16b4d70229428fee7e
SanjeQi <cristi.nord@gmail.com>
2021-02-06 18:14:06 +0000

    JQuery: detecting a click

---
+++ java_script.js
@@ -0,0 +1,12 @@
+// Select an id
+$('circle').click(function () {
+  alert('id Clicked!!');
+});
+// Selecti  a class
+$('.box').click(function () {
+  alert('class Clicked!!');
+});
+// Select  an element
+$('div').click(function () {
+  alert('div Clicked!!');
+});
