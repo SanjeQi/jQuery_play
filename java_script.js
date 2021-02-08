5543f6a40fd213f785d262cbe0de72d1f5d72ff3
SanjeQi <cristi.nord@gmail.com>
2021-02-08 17:22:12 +0000

    JQuery: Loading data with AJAX

--- java_script.js
+++ java_script.js
@@ -1,14 +1,30 @@
-// Animating content
+// Loading data with AJAX
 
-$('#circle').click(function () {
-  $(this).css('backgroundColor', 'red');
-  $(this).animate(
-    {
-      width: '500px',
-      height: '500px',
-      marginLeft: '300px',
-      marginTop: '500px',
-    },
-    800
-  );
+// jQuery get function
+$.get('test.html', function (data) {
+  console.log(data);
+});
+
+//jQuery ajax function
+$.ajax({ url: 'test.html' }).done(function (data) {
+  console.log(data);
+});
+
+//jQuery after an ajax call change the h1 content
+$.ajax({ url: 'test.html' }).done(function (data) {
+  $('h1').html(data);
+});
+
+//jQuery changing text after an ajax call
+$.ajax({ url: 'test.html' }).done(function (data) {
+  $('#btn1').click(function () {
+    $('h1').html(data);
+  });
+});
+
+//jQuery appending text after an ajax call
+$.ajax({ url: 'test.html' }).done(function (data) {
+  $('#btn1').click(function () {
+    $('h1').append(data);
+  });
 });
