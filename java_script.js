22c752e5d535706117c13b816c4c071b488f6dd1
SanjeQi <cristi.nord@gmail.com>
2021-02-08 16:08:03 +0000

    JQuery: Animating content

--- java_script.js
+++ java_script.js
@@ -1,39 +1,14 @@
-//********** Click on the button to fadeOut/fadeIn the p tag
-$('#btn1').click(function () {
-  $('p').fadeOut();
-});
+// Animating content
 
-$('#btn2').click(function () {
-  $('p').fadeIn();
-});
-
-//********** Changing the fade speed v1
-$('#btn1').click(function () {
-  $('p').fadeOut(500);
-});
-
-$('#btn2').click(function () {
-  $('p').fadeIn(600);
-});
-
-//********** Changing the fade speed v2
-$('#btn1').click(function () {
-  $('p').fadeOut('slow');
-});
-
-$('#btn2').click(function () {
-  $('p').fadeIn('fast');
-});
-
-//********** Changing the fade with a callback function
-$('#btn1').click(function () {
-  $('p').fadeOut('slow', function () {
-    console.log('Fade out complete');
-  });
-});
-
-$('#btn2').click(function () {
-  $('p').fadeIn('slow', function () {
-    console.log('Fade In complete');
-  });
+$('#circle').click(function () {
+  $(this).css('backgroundColor', 'red');
+  $(this).animate(
+    {
+      width: '500px',
+      height: '500px',
+      marginLeft: '300px',
+      marginTop: '500px',
+    },
+    800
+  );
 });
