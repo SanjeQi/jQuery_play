85f9e41d2f122184f0e691495ad23b1db693cdf1
SanjeQi <cristi.nord@gmail.com>
2021-02-10 16:44:49 +0000

    JQueryUI: Drop  droppable()

--- java_script.js
+++ java_script.js
@@ -6,3 +6,18 @@ $('#square2').resizable();
 
 //Drag and Resize
 $('#square3').draggable().resizable();
+
+//Drop - droppable()
+
+$('#square4').droppable({
+  drop: function (ui, event) {
+    console.log('dropped');
+  },
+});
+
+//Drop - droppable()  and change color on drop
+$('#square4').droppable({
+  drop: function (ui, event) {
+    $(this).css('background-color', 'yellow');
+  },
+});
