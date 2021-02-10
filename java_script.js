d79fdd6c95c96fa78baf5dc038f1d1791253385d
SanjeQi <cristi.nord@gmail.com>
2021-02-10 17:04:52 +0000

    JQueryUI:  Accordion and Sortable

--- java_script.js
+++ java_script.js
@@ -1,23 +1,5 @@
-//Draggable - draggable()
-$('#square1').draggable();
+//Accordion - accordion()
+$('#container').accordion();
 
-//Resizable - resizable()
-$('#square2').resizable();
-
-//Drag and Resize
-$('#square3').draggable().resizable();
-
-//Drop - droppable()
-
-$('#square4').droppable({
-  drop: function (ui, event) {
-    console.log('dropped');
-  },
-});
-
-//Drop - droppable()  and change color on drop
-$('#square4').droppable({
-  drop: function (ui, event) {
-    $(this).css('background-color', 'yellow');
-  },
-});
+//Sortable - sortable()
+$('ul').sortable();
