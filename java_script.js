da788beda4f3c21c83ac14400fa7c0277d9fa608
SanjeQi <cristi.nord@gmail.com>
2021-02-10 15:17:42 +0000

    JQueryUI: Drag and resize

--- java_script.js
+++ java_script.js
@@ -1,1 +1,8 @@
-// Form validation
+//Draggable - draggable()
+$('#square1').draggable();
+
+//Resizable - resizable()
+$('#square2').resizable();
+
+//Drag and Resize
+$('#square3').draggable().resizable();
